5fc6ce4c3643e8d55e7f3e35e57745a568cb0014
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-23 16:17:27 -0600

    delete movie

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ const App = (props) => {
       });
   }, []);
 
-  const deleteMovie = (id) => {};
+  const deleteMovie = (id) => {
+    setMovies(movies.filter((item) => item.id !== Number(id)));
+  };
 
   const addToFavorites = (movie) => {};
 
@@ -46,7 +48,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie />
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
